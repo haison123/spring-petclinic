8c5ef6bfcf821725fa31c0fe4387ab47deb139c5
haison123 <51000239+haison123@users.noreply.github.com>
2024-04-11 00:56:40 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-export APP_PID=$(ps -ef | grep 'java -jar /home/ubuntu/spring-petclinic' | awk '{print $2}' | head -n 1)
-if [ -z "$APP_PID" ]; then echo "[INFO] APPLICATION IS STARTING..."; else echo "[INFO] APPLICATION IS RUNNING WITH PID IS: $APP_PID"; echo "[INFO] STOPPING THE OLD VERSION..."; kill -9 $APP_PID; fi
+
+sudo su 
+
+export APP_PID=$(ps -ef | grep '[j]ava -jar /home/ubuntu/spring-petclinic' | awk '{print $2}')
+if [ -z "$APP_PID" ]; then
+    echo "[INFO] APPLICATION IS STARTING..."
+else
+    echo "[INFO] APPLICATION IS RUNNING WITH PID(S): $APP_PID"
+    echo "[INFO] STOPPING THE OLD VERSION..."
+    for pid in $APP_PID; do
+        kill -9 $pid
+    done
+fi
+
 echo "[INFO] STARTING THE NEW VERSION OF APPLICATION..."
 nohup java -jar /home/ubuntu/spring-petclinic-*.jar &
+
 sleep 15
+
 export APP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/actuator/health)
-if [ "$APP_STATUS" == "200" ]; then echo "[INFO] APPLICATION STARTED SUCCESSFULLY..."; else echo "[ERROR] UNKNOWN ERROR"; fi
+if [ "$APP_STATUS" == "200" ]; then
+    echo "[INFO] APPLICATION STARTED SUCCESSFULLY..."
+else
+    echo "[ERROR] UNKNOWN ERROR"
+fi
