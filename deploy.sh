6fe0a206ca6ca6475a2de8e0f3ba5298378af8b8
haison123 <51000239+haison123@users.noreply.github.com>
2024-04-10 19:13:38 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-!#/bin/sh
+#!/bin/bash
 export APP_PID=$(ps -ef | grep 'java -jar /home/ubuntu/spring-petclinic' | awk '{print $2}' | head -n 1)
 if [ -z "$APP_PID" ]; then echo "[INFO] APPLICATION IS STARTING..."; else echo "[INFO] APPLICATION IS RUNNING WITH PID IS: $APP_PID"; echo "[INFO] STOPPING THE OLD VERSION..."; kill -9 $APP_PID; fi
 echo "[INFO] STARTING THE NEW VERSION OF APPLICATION..."
